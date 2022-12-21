67ae5014da39d39cb6d5af56908d0f3b868b4025
Do Hoon Kim <op@kdh.io>
2022-12-21 12:54:16 +0900

    Update

--- src/bot/sendVerifyMessage.js
+++ src/bot/sendVerifyMessage.js
@@ -19,7 +19,8 @@ module.exports = async function sendVerifyMessage(guild, user, channelId, messag
                 } else {
                     let message = getLocale(serverSettings.language, "userEnterEmail", ("(<name>" + serverSettings.domains.toString().replaceAll(",", "|").replaceAll("*", "\\*") + ")"))
                     if (serverSettings.logChannel !== "") {
-                        message += " Caution: The admin can see the used email address"
+                        // message += " Caution: The admin can see the used email address"
+                        message += "> 계속 진행할 경우 SketchTown 운영정책에 동의한 것으로 간주됩니다."
                     }
                     await user.send(message).catch(() => {
                     })
